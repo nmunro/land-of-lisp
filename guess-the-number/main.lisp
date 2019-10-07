2b781992200abe4d615f39c8ded6ae546911ecfd
Neil Munro <neilmunro@gmail.com>
2019-10-07 09:01:39 +0100

    Added basic game engine

--- guess-the-number/main.lisp
+++ guess-the-number/main.lisp
@@ -1,18 +1,18 @@
-(defparameter *small* 1)
-(defparameter *big* 1024)
+(defparameter *upper* 1024)
+(defparameter *lower* 1)
 
 (defun guess-my-number ()
-  (ash (+ *small* *big*) -1))
+  (ash (+ *lower* *upper*) -1))
 
 (defun smaller ()
-  (setf *big* (1- (guess-my-number)))
+  (setf *upper* (1- (guess-my-number)))
   (guess-my-number))
 
 (defun bigger ()
-  (setf *small* (1+ (guess-my-number)))
+  (setf *lower* (1+ (guess-my-number)))
   (guess-my-number))
 
 (defun start-over ()
-  (defparameter *small* 1)
-  (defparameter *big* 1024)
+  (defparameter *upper* 1024)
+  (defparameter *lower* 1)
   (guess-my-number))
